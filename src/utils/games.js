8fe59eaeffce093f64149952ff3cf872fb1fbb4c
unknown <jbroa22@gmail.com>
2023-02-04 21:32:07 +0800

    add how to play, deposits and withdrawals and tournaments page

---
+++ src/utils/games.js
@@ -0,0 +1,62 @@
+export const games = [
+    {
+        "title": "Royal Flush",
+        "description": "Poker’s most famous hand, a royal flush, cannot be beaten. It consists of the ace, king, queen, jack and ten of a single suit.",
+        "image": "royal-flush.png",
+        "alt": "royal flush game",
+    },
+    {
+        "title": "Straight Flush",
+        "description": "Five cards in sequence, of the same suit. In the event of a tie, the highest rank at the top of the sequence wins.",
+        "image": "straight-flush.png",
+        "alt": "straight flush game",
+    },
+    {
+        "title": "Four of a Kind",
+        "description": "Four cards of the same rank, and one side card or ‘kicker’. In the event of a tie, the player with the highest side card (‘kicker’) wins.",
+        "image": "four-of-a-kind.png",
+        "alt": "four of a kind game",
+    },
+    {
+        "title": "Full House",
+        "description": "Three cards of the same rank, and two cards of a different, matching rank. In the event of a tie, the highest three matching cards wins.",
+        "image": "full-house.png",
+        "alt": "full house game",
+    },
+    {
+        "title": "Flush",
+        "description": "Five cards of the same suit, not in sequence. In the event of a tie, the player holding the highest ranked card wins.",
+        "image": "flush.png" ,
+        "alt": "flush game",
+    },
+    {
+        "title": "Straight",
+        "description": "Five non-suited cards in sequence. In the event of a tie, the highest ranking card at the top of the sequence wins.",
+        "image": "straight.png",
+        "alt": "straight game",
+    },
+    {
+        "title": "Three of a Kind",
+        "description": "Three cards of the same rank, and two unrelated side cards. In the event of a tie, the player with the highest, and if necessary, second-highest side card (‘kicker’) wins.",
+        "image": "three-of-a-kind.png",
+        "alt": "three of a kind game",
+    },
+    {
+        "title": "Two Pair",
+        "description": "Two cards of matching rank, two cards of different matching rank, and one kicker. If both players have an identical Two Pair, the highest kicker wins.",
+        "image": "two-pair.png",
+        "alt": "two pair game",
+    },
+    {
+        "title": "Pair",
+        "description": "Two cards of matching rank, and three unrelated side cards. In the event of a tie, the player with the highest, and if necessary, second or third-highest side card wins.",
+        "image": "pair.png",
+        "alt": "pair game",
+    },
+    {
+        "title": "High Card",
+        "description": "Any hand that does not qualify under the categories listed. In the event of a tie, the highest card wins, such as ‘ace-high’.",
+        "image": "high-card.png",
+        "alt": "high card game",
+    },
+];
---
+++ src/utils/payments.js
@@ -0,0 +1,62 @@
+export const PaymentMethods = [
+    {
+        "name": "applepay",
+        "deposit": true,
+        "withdraw": true,
+    },
+    {
+        "name": "rapid",
+        "deposit": true,
+        "withdraw": false,
+    },
+    {
+        "name": "skrill",
+        "deposit": true,
+        "withdraw": true,
+    },
+    {
+        "name": "neteller",
+        "deposit": true,
+        "withdraw": true,
+    },
+    {
+        "name": "visa",
+        "deposit": true,
+        "withdraw": true,
+    },
+    {
+        "name": "mastercard",
+        "deposit": true,
+        "withdraw": false,
+    },
+    {
+        "name": "local",
+        "deposit": true,
+        "withdraw": false,
+    },
+    {
+        "name": "direct",
+        "deposit": true,
+        "withdraw": true,
+    },
+    {
+        "name": "wiretransfer",
+        "deposit": false,
+        "withdraw": true,
+    },
+    {
+        "name": "muchbetter",
+        "deposit": false,
+        "withdraw": true,
+    },
+    {
+        "name": "online",
+        "deposit": true,
+        "withdraw": false,
+    },
+    {
+        "name": "trustly",
+        "deposit": true,
+        "withdraw": false,
+    }
+];
